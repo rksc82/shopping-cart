7aa2dcccb74dbdb124b94f032bff1c0b1fdc4f43
rahulsingh2507 <rahulsingh2507@gmail.com>
2018-02-12 01:33:59 -0600

    update docker

--- localBuildAndRun.sh
+++ localBuildAndRun.sh
@@ -3,7 +3,8 @@
 #Script will build and add jar to the lib folder
 ./gradlew clean build
 
-echo "##### Building uptake-noc-tasks-api image #####"
+#building the image
 docker build -t rks/shopping-cart .
 
+#running the container
 docker run  -p 8080:8080 rks/shopping-cart
